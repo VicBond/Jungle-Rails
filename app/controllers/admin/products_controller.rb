9f1cb1dbdfe35c5e2a27bd09d0b5de72c38c4dc5
Victor Bondarenko <nirasimha@gmail.com>
2022-01-05 20:56:58 +0000

    Admin Authentication added for both pages

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,9 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with  name: ENV["HTTP_ADMIN_USER"],
+                                password: ENV["HTTP_ADMIN_PASSWORD"],
+                                if: -> { ENV["HTTP_ADMIN_PASSWORD"].present? }
+
   def index
     @products = Product.order(id: :desc).all
   end
