2be8fe1d0a27d45e853b01d7835f62999208effb
Victor Bondarenko <nirasimha@gmail.com>
2022-01-10 20:44:42 +0000

    add a subject to specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+    subject {
+      described_class.new(name: "Product Test",
+                          price: 42,
+                          quantity: 42,
+                          category: Category.new)
+    }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    
+  end
+  
 end
