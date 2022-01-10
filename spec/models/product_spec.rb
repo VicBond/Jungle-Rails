1dd9e067ad859ec15b54800a1e13ab05c653202f
Victor Bondarenko <nirasimha@gmail.com>
2022-01-10 20:50:29 +0000

    validation product fields being tested to nil

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,7 +13,23 @@ RSpec.describe Product, type: :model do
     it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
-    
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without a price" do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without a quantity" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+    it "is not valid without a category" do
+      subject.category = nil
+      expect(subject).to_not be_valid
+    end
   end
   
 end
