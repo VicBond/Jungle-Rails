245a3f2e7928a6868dcb7e3a20b989538f791745
Victor Bondarenko <nirasimha@gmail.com>
2022-01-11 17:54:32 +0000

    test that users can navigate from the home page to the product detail page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates from the home page to the product detail page by clicking on a product", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product details page after clicking on a product image" do
+      # ACT
+      visit root_path
+
+      page.find('a.btn-default', match: :first).click
+      
+      # VERIFY
+      expect(page).to have_css 'article.product-detail'
+      
+      # DEBUG
+      save_screenshot
+  end
+
 end
